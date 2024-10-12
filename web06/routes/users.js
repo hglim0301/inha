a9bd8a2c01bed22af59e705f856c1801542b99f2
hglim0301 <limhan0301@gmail.com>
2024-10-12 18:50:58 +0900

    :sparkles: Add Week06
    6주차 과제

---
+++ web06/routes/users.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource'); // res.send - 데이터 전송
+});
+
+//로그인
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: '로그인', pageName: 'users/login.ejs' }); // 페이지 띄우기
+});
+
+//회원가입
+router.get('/join', function(req, res, next) {
+  res.render('index', { title: '회원가입', pageName: 'users/join.ejs' }); // 페이지 띄우기
+});
+
+module.exports = router;
